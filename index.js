fe15b7a6466750f19796f8847fd9840e08b6bb78
Bryant Lin <bryantmlin@gmail.com>
2023-03-21 14:49:50 -0600

    array filter 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(collection,string) {
+    return collection.filter((x) => x.toLowerCase() === string.toLowerCase())
+    }
+
+function fuzzyMatch(collection,string) {
+    return collection.filter((element) => element.substring(0,string.length) === string)
+}
+
+function matchName (collection,string) {
+    return collection.filter((element) => element.name === string)
+}
